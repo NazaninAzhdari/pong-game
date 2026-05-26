07668b25b8d90f01eb8f3107ec4b5885a9a5015b
Naz <Nazanin.Azhdari.sbu@gmail.com>
2026-05-26 15:27:26 +0300

    editing i2s transmitter, resolving some small problem in transmitting audio data

--- i2s_tx.vhd
+++ i2s_tx.vhd
@@ -4,44 +4,51 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity i2s_tx is
     port (
-        i_clk         :       in      STD_LOGIC;            --50 MHz 
+        i_clk         :       in      STD_LOGIC;            --FPGA Clock = 50 MHz 
         i_sample      :       in      unsigned(23 downto 0);
-        o_XCLK        :       out     STD_LOGIC;            --12.5 MHz
-        o_LRCLK       :       out     STD_LOGIC;            --48 KHz
-        o_BCLK        :       out     STD_LOGIC;            --3.1 MHz = 48KHz * 2 * 32
+        o_XCLK        :       out     STD_LOGIC;            --Required Master Clock for running the DAC= 12.5 MHz
+        o_LRCLK       :       out     STD_LOGIC;            --The LRCLK = Sample rate = 48 KHz => The LRCLK is high for right Channel and its low for left channel
+        o_BCLK        :       out     STD_LOGIC;            --Bit CLK = Sample Rate * number of bits per channel * channels = 3.1 MHz = 48KHz * 32 * 2
         o_DATA        :       out     STD_LOGIC
     );
 end i2s_tx;
 
 architecture RTL of i2s_tx is
     --XCLK 
-    constant    CLK_CYCLES_XCLK     :   integer     :=3;
+    constant    c_HALF_PERIOD_XCLK  :   integer     :=2;    --Number of Clock cycles required for half period of the Master Clock(12.5MHz).
+                                                            --System Clock / (Master CLK * 2)
     signal      r_XCLK              :   STD_LOGIC   :='0';
 
     --Bit CLK
-    constant    CLK_CYCLES_BCLK     :   integer     :=9;
+    constant    c_HALF_PERIOD_BCLK  :   integer     :=8;    --System Clock / (Master CLK * 2)
     signal      r_BCLK              :   STD_LOGIC   :='0';
 
     --LR clock
     signal      r_LRCLK             :   STD_LOGIC   :='1';
-
     signal      r_shift             :   unsigned(31 downto 0)   :=(others=>'0');
-    signal      r_bit_counter       :   integer range 0 to 31   :=0;
+    signal      r_bit_counter       :   integer range 0 to 31   :=31;
 
     begin
 
+        ------------------------------------
+        --Generating 12.5 MHz Master Clock
+        ------------------------------------
         generating_master_clock_12MHz : entity work.freq_divider
         generic map(
-            g_CLK_CYCLES => CLK_CYCLES_XCLK
+            g_HALF_PERIOD_OUT_FREQ => c_HALF_PERIOD_XCLK
         )
         port map (
             i_clk => i_clk,  --50 MHz 
             o_clk => r_XCLK  --12.5 MHz
         );
 
+        -------------------------------------------------------------------------------------------
+        --Generating 3.1 MHz Bit Clock
+        --Bit Clock = sample rate(48KHz) * number of bits per channel(32) * number of channels(2)
+        -------------------------------------------------------------------------------------------
         generating_bit_clock_3MHz : entity work.freq_divider
         generic map(
-            g_CLK_CYCLES => CLK_CYCLES_BCLK
+            g_HALF_PERIOD_OUT_FREQ=> c_HALF_PERIOD_BCLK
         )
         port map (
             i_clk => i_clk,  --50 MHz
